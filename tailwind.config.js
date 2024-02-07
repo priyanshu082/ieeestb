5160a2a56a128d12ea42fd81a8dba743b7050b4f
priyanshu082 <priyanshusingh216@gmail.com>
2024-02-07 09:36:18 +0530

    tailwind is changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,12 +29,12 @@ module.exports = {
           secondary:"#000000"
         },
       },
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
+      // screens: {
+      //   'sm': '640px',
+      //   'md': '768px',
+      //   'lg': '1024px',
+      //   'xl': '1280px',
+      // },
     },
   },
   plugins: [],
